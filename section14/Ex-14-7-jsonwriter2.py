249c1b290922d815bfb838283395ce80ae42d5c5
ITSC <ITSC@example.com>
2023-06-17 10:07:49 +0900

    추가

---
+++ section14/Ex-14-7-jsonwriter2.py
@@ -0,0 +1,26 @@
+import json
+
+dict_list = [
+    {
+        'name' : 'james',
+        'age':20,
+        'spec':[
+            175.5,
+            70.5
+        ]
+    },
+    {
+        'name':'홍길동',
+        'age':21,
+        'spec':[
+            168.5,
+            60.5
+        ]
+    }
+]
+# indent 들여쓰기, ensure_ascii_
+json_string = json.dumps(dict_list, indent=4, ensure_ascii=False)
+with open('dictList.json', 'w', encoding='UTF-8') as file:
+    file.write(json_string)
+print('dictList.json파일이 생성되었습니다')
+
