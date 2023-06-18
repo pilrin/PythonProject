cc2f039e707b7dcf74aa7227236dd65cccd404d0
pilrin <kyu3729@naver.com>
2023-06-18 12:46:31 +0900

    추가

--- section18/Ex18-2-beautifulsoup.py
+++ section18/Ex18-2-beautifulsoup.py
@@ -5,4 +5,19 @@ BeautifulSoup
 
 BeautifulSoup 설치방법
 pip install beautifulsoup
+
+https://
 '''
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://news.nate.com/rank'
+param = {'mid': 'n1000'}
+response = requests.get(url, params=param)
+html = response.text
+
+soup = BeautifulSoup(html, 'html.parser')
+tit_list = soup.find_all('h2', class_='tit')
+for tit in tit_list:
+    print(tit.text.strip())
