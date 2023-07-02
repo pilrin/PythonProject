cf5cd633f596d40f4254f5e0cf606d3159b67757
ITSC <ITSC@example.com>
2023-07-02 10:08:50 +0900

    추가

--- section19/Ex19-3-matplolib3.py
+++ section19/Ex19-3-matplolib3.py
@@ -9,8 +9,8 @@ rc('font', family=font_name)
 figure = plt.figure()
 axes = figure.add_subplot(111)
 data = [0.18, 0.3, 3.33, 3.75, 0.38, 25, 0.25, 2.75, 0.1]
-vitamin = ['비타민 A', '비타민 B1', '비타민 B2', '나이아신', '비타민 B6',
-            '비타민 C', '비타민 D', '비타민 E', '엽산']
+vitamin = {'비타민 A', '비타민 B1', '비타민 B2', '나이아신', '비타민 B6',
+           '비타민 C', '비타민 D', '비타민 E', '엽산'}
 axes.pie(data, labels=vitamin, autopct='%0.1f%%')
 plt.axis('equal')
 plt.show()
