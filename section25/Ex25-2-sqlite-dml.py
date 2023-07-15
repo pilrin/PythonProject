0f822a14fad7c924095785797d8d87c2cf02c905
pilrin <kyu3729@naver.com>
2023-07-15 12:55:24 +0900

    추가

---
+++ section25/Ex25-2-sqlite-dml.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+conn = sqlite3.connect('hr.db')
+cur = conn.cursor()
+
+#데이터 삽입
+sql = "INSERT INTO employees" \
+      " (first_name, last_name," \
+      " email, hire_data, job_id, salary, department_id) " \
+      "VALUES (?,?,?,?,?,?,?)"
+cur.execute(sql,('John','Doe','johndoe1@exmple.com,'
+                              '2023-07-15','IT_PROG', 5000, 90))
+conn.commit()
+cur.close()
+conn.close() 
+
+
+#데이터 조회
+conn = sqlite3.connect('hr.db')
+cur = conn.cursor()
+sql = "SELECT * FROM employees WHERE employee_id = ?"
+cur.execute(sql,(1,))
+rows = cur.fetchall()
+print(rows)
+
+#데이터 수정
+conn = sqlite3.connect('hr.db')
+cur = conn.cursor()
+sql = "UPDATE employees SET salary = ? WHERE employee_id = ?"
+cur.execute(sql,(5500, 1))
+conn.commit()
+
+cur.close()
+conn.close()
+
+#데이터 삭제
+conn = sqlite3.connect('hr.db')
+c0ur = conn.cursor()
+sql = "DELETE FROM employees"
+cur.execute(sql)
+conn.commit()
+
+cur.close()
+conn.close()
+
+
+
